a37d53adfdeeec610472afe980fa8b488f06df66
Aiden <aiden@vigue.me>
2026-04-05 22:44:23 -0400

    IDF 6.0 GA

--- include/kd_crypto.h
+++ include/kd_crypto.h
@@ -12,33 +12,37 @@
 extern "C" {
 #endif
 
-typedef struct esp_ds_data_ctx {
-    esp_ds_data_t* esp_ds_data;
-    uint8_t efuse_key_id;
-    uint16_t rsa_length_bits;
-} esp_ds_data_ctx_t;
+#ifdef CONFIG_MBEDTLS_HARDWARE_RSA_DS_PERIPHERAL
+#include "psa/crypto.h"
+#else
+    typedef struct esp_ds_data_ctx {
+        esp_ds_data_t* esp_ds_data;
+        uint8_t efuse_key_id;
+        uint16_t rsa_length_bits;
+    } esp_ds_data_ctx_t;
+#endif
 
 #define KD_CRYPTO_MAX_CLAIM_TOKEN_SIZE 256
 
-typedef enum CryptoState_t {
-    CRYPTO_STATE_UNINITIALIZED,
-    CRYPTO_STATE_KEY_GENERATED,
-    CRYPTO_STATE_VALID_CSR,
-    CRYPTO_STATE_VALID_CERT,
-    CRYPTO_STATE_BAD_DS_PARAMS,
-} CryptoState_t;
+    typedef enum CryptoState_t {
+        CRYPTO_STATE_UNINITIALIZED,
+        CRYPTO_STATE_KEY_GENERATED,
+        CRYPTO_STATE_VALID_CSR,
+        CRYPTO_STATE_VALID_CERT,
+        CRYPTO_STATE_BAD_DS_PARAMS,
+    } CryptoState_t;
 
-// Crypto functions
-esp_ds_data_ctx_t* kd_common_crypto_get_ctx(void);
-esp_err_t kd_common_get_device_cert(char* buffer, size_t* len);
-esp_err_t kd_common_set_device_cert(const char* cert, size_t len);
-esp_err_t kd_common_get_csr(char* buffer, size_t* len);
-esp_err_t kd_common_get_claim_token(char* buffer, size_t* len);
-esp_err_t kd_common_clear_claim_token(void);
+    // Crypto functions
+    esp_ds_data_ctx_t* kd_common_crypto_get_ctx(void);
+    esp_err_t kd_common_get_device_cert(char* buffer, size_t* len);
+    esp_err_t kd_common_set_device_cert(const char* cert, size_t len);
+    esp_err_t kd_common_get_csr(char* buffer, size_t* len);
+    esp_err_t kd_common_get_claim_token(char* buffer, size_t* len);
+    esp_err_t kd_common_clear_claim_token(void);
 
-CryptoState_t kd_common_crypto_get_state(void);
-bool kd_common_crypto_will_generate_key(void);
-esp_err_t kd_common_crypto_test_ds_signing(void);  // Debug: test DS peripheral signing
+    CryptoState_t kd_common_crypto_get_state(void);
+    bool kd_common_crypto_will_generate_key(void);
+    esp_err_t kd_common_crypto_test_ds_signing(void);  // Debug: test DS peripheral signing
 
 #ifdef __cplusplus
 }
